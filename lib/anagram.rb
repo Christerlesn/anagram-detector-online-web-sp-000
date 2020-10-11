746778b6e7b38fb76eb9180df4cbf360e6e75613
Christerle Nounoute <christerlesn@gmail.com>
2020-10-11 04:54:33 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 class Anagram
 attr_accessor :main_word
 
@@ -7,12 +7,8 @@ def initialize(main_word)
 end
 
 def match(array_of_words)
-
   array_of_words.select do |element|
     @main_word.split("").sort == element.split("").sort
   end
 end
-
-end #end of class Anagram
-
-binding.pry
+end
