584535a3be76179c9b49580ea7d4d6cdb532bcf2
Christerle Nounoute <christerlesn@gmail.com>
2020-10-11 03:04:08 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+attr_accessor :main_word
+
+def initalize(main_word)
+  @main_word = main_word
+end
+
+def match
+
+end
+
+
+
+
+
+
+end #end of class Anagram
