ec8ae9c2a7dc439f348ab20ba629966ada5a2c87
Christerle Nounoute <christerlesn@gmail.com>
2020-10-11 04:48:14 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,12 +7,12 @@ def initalize(main_word)
 end
 
 def match(array_of_words)
-  list = []
+#  list = []
   array_of_words.select do |word|
     @main_word.split("").sort == word.split("").sort
-    list << word
+#    list << word
   end
-list
+#list
 end
 
 
