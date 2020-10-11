1f3556e735802747fbe63f977b1db52aaad1797c
Christerle Nounoute <christerlesn@gmail.com>
2020-10-11 04:44:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,13 +7,13 @@ def initalize(main_word)
 end
 
 def match(array_of_words)
-  #It should return all matches in an array. 
+  #It should return all matches in an array.
   #If no matches exist, it should return an empty array.
   # determine if one word is an anagram for another?
   #1. iterate over the array of words
   #2. compare each word of that array to the word that the Anagram class is initialized with.
-  #3. To determine if they are anagrams, try determining if they are composed of the same letters. 
-  #4. Remember that you can split a word into an array of letters using some_word.split(""). 
+  #3. To determine if they are anagrams, try determining if they are composed of the same letters.
+  #4. Remember that you can split a word into an array of letters using some_word.split("").
     # You can compare two arrays using the ==
   #5. Two arrays are equal if they contain the same elements, in the same order.
   # Remember that you can .sort an arrays elements.
